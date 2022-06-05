7bf96c7662d0ad37ac2a41a9eccddddb28bdedc1
Kevin Howbrook <kevin.howbrook@torchbox.com>
2022-06-05 23:25:32 +0100

    Coverage

--- wagtail_testing/settings/base.py
+++ wagtail_testing/settings/base.py
@@ -24,8 +24,8 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 # Application definition
 
 INSTALLED_APPS = [
-    "home",
-    "search",
+    "wagtail_testing.home",
+    "wagtail_testing.search",
     "wagtail.contrib.forms",
     "wagtail.contrib.redirects",
     "wagtail.embeds",
